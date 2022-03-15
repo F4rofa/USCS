ef7d38e026a0e3727eac6e29098e5e56c1926ea2
F4rofa <76708679+F4rofa@users.noreply.github.com>
2022-03-15 13:01:25 -0300

    Update Atividade06.c

--- Atividade06.c
+++ Atividade06.c
@@ -1,7 +1,7 @@
 /*                      Atividade 6
-Algoritmos e Estrutura de Dados I - CiÍncia da ComputaÁ„o
-Prof. Dr. Aparecido Freitas (Cid„o)
-Feito por: Davi MarÁal
+Algoritmos e Estrutura de Dados I - Ci√™ncia da Computa√ß√£o
+Prof. Dr. Aparecido Freitas (Cid√£o)
+Feito por: Davi Mar√ßal
 */
 
 #include <stdio.h>
@@ -13,10 +13,11 @@ int main (void)
 	
 	int side1_of_rectangle = 5, side2_of_rectangle = 10, perimeter;
 	
-	printf("A altura do ret‚ngulo È %d.\n", side1_of_rectangle);
-	printf("A base do ret‚ngulo mede %d.\n", side2_of_rectangle);
+	printf("A altura do ret√¢ngulo √© %d.\n", side1_of_rectangle);
+	printf("A base do ret√¢ngulo mede %d.\n", side2_of_rectangle);
 	
 	perimeter = (2 * side1_of_rectangle) + (2 * side2_of_rectangle);
 	
-	printf("O perÌmetro do ret‚ngulo È de %d.", perimeter);
+	printf("O per√≠metro do ret√¢ngulo √© de %d.", perimeter);
+	return 0;
 }
